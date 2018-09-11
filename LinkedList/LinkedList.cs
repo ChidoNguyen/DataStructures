445cdfe2e33ffe4281d3e7fbeb2f228bbd85df51
Chido Nguyen <chidonguyen@hotmail.com>
2018-09-11 15:52:13 -0700

    Added C# file

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,14 @@
+//C# coding practice and datastructure review
+using System;
+//internal is equivalent to private
+namespace LinkedList {
+    class Nodes {
+        int data = null;
+		int nextNode = null;
+            }
+    static void Main()
+    {
+        Console.WriteLine("Hello");
+    }
+}
+	
